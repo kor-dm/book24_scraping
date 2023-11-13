7d83bce3c1a81704d7f8f96d8d8fe4732f939b86
Dmitry Korablin <korablindm@gmail.com>
2023-11-13 22:09:32 +0300

    Renamed write_data file, added a docstring to configs parser

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,4 @@
 from .configs_parsing import parse_configs
 from .site_parsing import parse_site, parse_item, parse_page
-from .write_data import setup_dir, write_data
+from .writing_data import setup_dir, write_data
 from .logging import log_cli, no_log
--- src/configs_parsing.py
+++ src/configs_parsing.py
@@ -1,6 +1,9 @@
 from json import load
 
 def parse_configs(name: str) -> dict:
+    """
+    Read configuration from .json file in /configs folder
+    """
     name = "./configs/" + name + ".json"
     with open(name, "r") as conf:
         conf_data = load(conf)
